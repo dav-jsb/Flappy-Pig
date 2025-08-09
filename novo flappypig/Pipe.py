69a8d42ae4a04b3cffec653b636f51e31825a55f
YtBelll <ytbell@outlook.com>
2025-08-08 21:02:51 -0300

    comentarios para resoolver os canos

--- novo flappypig/Pipe.py
+++ novo flappypig/Pipe.py
@@ -1,17 +1,26 @@
 import pygame
 from GameObject import GameObject
 
-class Pipe(GameObject):
-    def __init__(self, x, y, is_top, speed,color):
+### pra quem for resolver isso, acho que talvez ao inves de criar um tubo pra cima
+#e depois outro menor e inverter (como esta agora) seja melhor criar um grante tubo
+# e abrir um espaço no meio
+
+class Pipe(GameObject): ### talvez mudar a estrutura dessa classe tbm ajude
+    def __init__(self, x, y, is_top, speed,color): 
+        ### x e y acho q são o centro do tubo
+        # is_top é uma variavel pra inverter se ele for pro lado de cima
+        # speed é a velocidade do mapa, e color é a cor do tubo
         height = 300
         gap = 150
         
         if is_top:
-            super().__init__(x, y - gap, 60, height, color)
+            super().__init__(x, y - gap, 60, height, color) 
+            # usa o construtor da classe mãe (GameObject) pra completar
             self.image = pygame.transform.flip(self.image, False, True)
+             # inverte se for top
         else:
             super().__init__(x, y + gap, 60, height, color)
-        
+            #faz a mesma coisa mas sem inverter
         self.speed = speed
     
     def update(self):
