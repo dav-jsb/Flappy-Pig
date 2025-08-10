f2f88289f7dcbfc6d28e4778c8f4b49656fa23e1
dlpinheiro0012 <davipinheiro1266@gmail.com>
2025-08-10 13:04:58 -0300

    Alteração na contabilização dos pontos

--- novo flappypig/GameState.py
+++ novo flappypig/GameState.py
@@ -6,14 +6,24 @@ class GameState:
 class GameManager:
     def __init__(self):
         self.state = GameState.MENU
-        self.score = 0
-        self.high_score = 0
-    
+        # Pontuação por tipo de item
+        self.score_blue = 0
+        self.score_red = 0
+        self.score_white = 0
+    #Função para resetar a pontuação após o término do jogo
     def reset(self):
-        self.score = 0
+        self.score_blue = 0
+        self.score_red = 0
+        self.score_white = 0
         self.state = GameState.PLAYING
     
     def game_over(self):
-        if self.score > self.high_score:
-            self.high_score = self.score
         self.state = GameState.GAME_OVER
+    # adiciona os itens à pontuação com base no tipo de item
+    def add_item_score(self, item_type):
+        if item_type == "blue":
+            self.score_blue += 1
+        elif item_type == "red":
+            self.score_red += 1
+        elif item_type == "white":
+            self.score_white += 1
