f027f4bfa9835d8768f5bd7472e955214e35dfbc
dlpinheiro0012 <davipinheiro1266@gmail.com>
2025-08-12 19:36:30 -0300

    Create Assets.py
    Criei uma uma função que recebe todos os arquivos de imagem e armazena em um dic, que será puxado na Main

---
+++ novo flappypig/Assets.py
@@ -0,0 +1,17 @@
+import pygame
+
+def load_assets():
+    assets = {}
+
+    # Itens
+    assets["blue_item"] = pygame.image.load("wizard powerup.png").convert_alpha()
+    assets["red_item"] = pygame.image.load("fire powerup.png").convert_alpha()
+    assets["white_item"] = pygame.image.load("spider powerup.png").convert_alpha()
+
+    # Skins do porco
+    assets["pig_default"] = pygame.image.load("vanilla pig.png").convert_alpha()
+    assets["pig_white"] = pygame.image.load("spider powerup.png").convert_alpha()
+    assets["pig_blue"] = pygame.image.load("wizardpig.png").convert_alpha()
+    assets["pig_red"] = pygame.image.load("blaze pig.png").convert_alpha()
+
+    return assets
