b16b0ab5f25bf7be5abb517f8b43d9504d9086a5
dlpinheiro0012 <davipinheiro1266@gmail.com>
2025-08-13 00:36:28 -0300

    Mais colecionáveis adicionados

--- novo flappypig/Assets.py
+++ novo flappypig/Assets.py
@@ -7,6 +7,8 @@ def load_assets():
     assets["blue_item"] = pygame.image.load("wizard powerup.png").convert_alpha()
     assets["red_item"] = pygame.image.load("fire powerup.png").convert_alpha()
     assets["white_item"] = pygame.image.load("spider powerup.png").convert_alpha()
+    assets['green_item'] = pygame.image.load("carrot.png").convert_alpha() #carrot
+    assets['yellow_item'] = pygame.image.load("meal.png").convert_alpha() # meal 
 
     # Skins do porco
     assets["pig_default"] = pygame.image.load("vanilla pig.png").convert_alpha()
